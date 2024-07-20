76a662351838c8e37236e34fe48cdccae179f324
Bonolo Lebepe <125119232+nolly-B@users.noreply.github.com>
2024-07-20 19:02:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,38 +5,38 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
 
     let isValid = true;
-    let messages = ["Error", "Not found"];
+    let messages = [];
 
     let username = document.getElementById("username").value.trim();
 
     if (username.length < 3) {
       isValid = false;
-      feedbackDiv.textContent = messages.push("Incorrect username");
+      feedbackDiv.textContent = messages.push(
+        "Username must be at least 3 characters long"
+      );
       return;
     }
     let feedbackDiv = document.getElementById("form-feedback");
 
     feedbackDiv.style.display = "block";
-    if (isValid === true) {
+    if (isValid) {
       feedbackDiv.textContent = "Registration successful";
       feedbackDiv.style.color = "#28a745";
-      return;
     } else {
-      feedbackDiv.innerHTML = messages.push("Incorrect details").join("<br/");
+      feedbackDiv.innerHTML = messages.join("<br/");
       feedbackDiv.style.color = "#dc3545";
     }
     let password = document.getElementById("password").value.trim();
-    if (password.length >= 8) {
+    if (password.length < 8) {
       isValid = false;
-      feedbackDiv.textContent = messages.push("Password invalid");
+      feedbackDiv.textContent = messages.push(
+        "Password must be atleast 8 characters long"
+      );
       return;
     }
     let email = document.getElementById("email").value.trim();
 
-    if (email.includes("@", ".")) {
-      isValid = true;
-      return;
-    } else {
+    if (!email.includes("@") || !email.includes(".")) {
       isValid = false;
       feedbackDiv.textContent = messages.push("Incorrect email format");
       return;
